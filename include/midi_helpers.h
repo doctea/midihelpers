614a390fcaa3369e5cbd456727a797539eb5a6d4
doctea <doctea@gmail.com>
2023-03-16 00:23:34 +0000

    quantise_pitch can handle an optional root note and scale number

--- include/midi_helpers.h
+++ include/midi_helpers.h
@@ -10,7 +10,22 @@ String get_note_name(int pitch);
 const char *get_note_name_c(int pitch);
 bool is_valid_note(int8_t byte);
 
-byte quantise_pitch(byte pitch);
+byte quantise_pitch(byte pitch, byte root_note, byte scale_number);
+
+
+#define SCALE_ROOT_C       0
+#define SCALE_ROOT_C_SHARP 1
+#define SCALE_ROOT_D       2
+#define SCALE_ROOT_D_SHARP 3
+#define SCALE_ROOT_E       4
+#define SCALE_ROOT_F       5
+#define SCALE_ROOT_F_SHARP 6
+#define SCALE_ROOT_G       7
+#define SCALE_ROOT_G_SHARP 8
+#define SCALE_ROOT_A       9
+#define SCALE_ROOT_A_SHARP 10
+#define SCALE_ROOT_B       11
+
 
 #endif
 
--- src/midi_helpers.cpp
+++ src/midi_helpers.cpp
@@ -16,6 +16,7 @@ String get_note_name(int pitch) {
   return s;
 }
 
+// todo: version that will return GM drum name
 const char *get_note_name_c(int pitch) {
   if (!is_valid_note(pitch)) {
     return "_";
@@ -34,23 +35,38 @@ bool is_valid_note(int8_t note) {
     return note >= 0 && note <= MIDI_MAX_NOTE;
 }
 
-// todo: version that will return GM drum name
+/*#define SCALE_ROOT_A       21
+#define SCALE_ROOT_A_SHARP 22
+#define SCALE_ROOT_B       23
+#define SCALE_ROOT_C       24
+#define SCALE_ROOT_C_SHARP 25
+#define SCALE_ROOT_D       26
+#define SCALE_ROOT_D_SHARP 27
+#define SCALE_ROOT_E       28
+#define SCALE_ROOT_F       29
+#define SCALE_ROOT_F_SHARP 30
+#define SCALE_ROOT_G       31
+#define SCALE_ROOT_G_SHARP 32*/
 
+#define PITCHES_PER_SCALE 7
 
-byte quantise_pitch(byte pitch) {
+byte quantise_pitch(byte pitch, byte root_note = SCALE_ROOT_A, byte scale_number = 0) {
   byte octave = pitch / 12;
   byte chromatic_pitch  = pitch % 12;
 
-  byte valid_chromatic_pitches[] = {
-    0, 2, 4, 5, 7, 9, 11
+  static const byte valid_chromatic_pitches[][PITCHES_PER_SCALE] = {
+    { 0, 2, 4, 5, 7, 9, 11 },
+    { 0, 2, 3, 5, 6, 9, 10 },
+    { 0, 1, 3, 4, 7, 9, 11 },
   };
+  byte root_note_mod = root_note % 12;
   
   byte nearest_lowest = 0;
-  for (int i = 0 ; i < sizeof(valid_chromatic_pitches) ; i++) {
-    if(valid_chromatic_pitches[i]==chromatic_pitch)
+  for (int i = 0 ; i < PITCHES_PER_SCALE ; i++) {
+    if( (root_note + valid_chromatic_pitches[scale_number][i]) % 12==chromatic_pitch)
       return pitch;
-    if (chromatic_pitch < valid_chromatic_pitches[i])
-      nearest_lowest = valid_chromatic_pitches[i-1];
+    if (chromatic_pitch < root_note + valid_chromatic_pitches[scale_number][i])
+      nearest_lowest = root_note + valid_chromatic_pitches[scale_number][i-1];
   }
   return nearest_lowest;
 }
