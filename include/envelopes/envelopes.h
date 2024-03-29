0310316eea089af847f224f8383b417ff562bc3d
doctea <doctea@gmail.com>
2024-03-29 00:36:31 +0000

    wip, not working/compiling yet - refactoring envelope classes to better allow different envelope types

--- include/envelopes/envelopes.h
+++ include/envelopes/envelopes.h
@@ -17,6 +17,8 @@
 
 class Menu;
 
+// todo: make EnvelopeBase accept a lambda as callback, instead of the send_envelope_level override
+
 class EnvelopeBase { 
     public:
 
@@ -52,16 +54,6 @@ class EnvelopeBase {
     int8_t actual_level = 0;          // right now, the level
     //int8_t stage_start_level = 0;     // level at start of current stage
 
-    // TODO: int delay_length = 5;                    // D - delay before atack starts
-    unsigned int  attack_length   = 0;                // A - attack  - length of stage
-    unsigned int  hold_length     = (PPQN / 4) - 1;   // H - hold    - length to hold at end of attack before decay
-    unsigned int  decay_length    = (PPQN / 2) - 1;   // D - decay   - length of stage
-    float         sustain_ratio   = 0.90f;            // S - sustain - level to drop to after decay phase
-    unsigned int  release_length  = (PPQN / 2) - 1;   // R - release - length (time to drop to 0)
-
-    int8_t lfo_sync_ratio_hold_and_decay = 0;
-    int8_t lfo_sync_ratio_sustain_and_release = 0;
-
     unsigned long stage_triggered_at = 0;
     unsigned long triggered_at = 0; 
     unsigned long last_sent_at = 0;
@@ -73,14 +65,121 @@ class EnvelopeBase {
     int8_t last_sent_lvl; // value but not inverted
     int8_t last_sent_actual_lvl;  // actual midi value sent
 
-    int8_t cc_value_sync_modifier = 12;
-
     /*void send_envelope_level(int8_t level) {
         output_wrapper->sendControlChange(midi_cc, level, channel);
     }*/
     virtual void send_envelope_level(uint8_t level) = 0;
 
-    void randomise() {
+    virtual void randomise() = 0;
+    virtual void initialise_parameters() = 0;
+
+    virtual void kill() {
+        this->stage = OFF;
+        //this->stage_start_level = (int8_t)0;
+        this->last_state.stage = OFF;
+        this->last_state.lvl_start = 0;
+        this->last_state.lvl_now = 0;
+        send_envelope_level(0);
+    }
+
+    // received a message that the state of the envelope should change (note on/note off etc)
+    virtual void update_state (int8_t velocity, bool state, uint32_t now = ticks) = 0;
+
+    struct envelope_state_t {
+        uint8_t stage = OFF;
+        uint8_t lvl_start = 0;
+        uint8_t lvl_now = 0;
+        uint32_t elapsed = 0;
+    };
+    envelope_state_t calculate_envelope_level(uint8_t stage, uint8_t stage_elapsed, uint8_t level_start, uint8_t velocity = 127) = 0;
+
+    struct graph_t {
+        int_least8_t value = 0;
+        char stage = -1;
+    };
+    graph_t graph[240];
+
+    void calculate_graph() {
+        envelope_state_t graph_state = {
+            .stage = ATTACK,
+            .lvl_start = 0,
+            .lvl_now = 0
+        };
+        int stage_elapsed = 0;
+        for (int i = 0 ; i < 240 ; i++) {
+            envelope_state_t result = calculate_envelope_level(graph_state.stage, stage_elapsed, graph_state.lvl_start, velocity);
+            if (result.stage != graph_state.stage) {
+                graph_state.lvl_start = result.lvl_now;
+                stage_elapsed = 0;
+            } else {
+                stage_elapsed++;
+            }
+            graph[i].value = result.lvl_now;
+            graph[i].stage = result.stage;
+            graph_state.stage = result.stage;
+            if (result.stage==SUSTAIN && stage_elapsed >= PPQN) {
+                stage_elapsed = 0;
+                graph_state.stage = RELEASE;    // move to release after 1 beat, if we are calculating the graph
+            }
+        }
+    }
+
+    envelope_state_t last_state = {
+        .stage = OFF,
+        .lvl_start = 0,
+        .lvl_now = 0,
+        .elapsed = 0
+    };
+    void process_envelope(unsigned long now = millis()) {
+        //now = ticks;
+        unsigned long elapsed = now - this->stage_triggered_at;
+        //unsigned long real_elapsed = elapsed;    // elapsed is currently the number of REAL ticks that have passed
+
+        envelope_state_t new_state = calculate_envelope_level(last_state.stage, elapsed, last_state.lvl_start, velocity);
+        if (new_state.stage!=last_state.stage) {
+            //Serial.printf("process_envelope(now=%-3i) began at stage %i, changed to stage %i (elapsed is %-3i)\n", now, last_state.stage, new_state.stage, elapsed);
+            this->stage_triggered_at = now;
+        }
+
+        if (this->last_sent_actual_lvl != new_state.lvl_now) {
+            send_envelope_level(new_state.lvl_now);
+            last_sent_actual_lvl = new_state.lvl_now;
+        }
+
+        last_state.elapsed = elapsed;
+        last_state.stage = new_state.stage;
+        last_state.lvl_start = new_state.lvl_start;
+        last_state.lvl_now = new_state.lvl_now;
+    }
+
+
+    
+    #ifdef ENABLE_SCREEN
+        virtual void make_menu_items(Menu *menu, int index);
+    #endif
+
+};
+
+
+
+class RegularEnvelope : public EnvelopeBase {
+    public:
+
+    RegularEnvelope(const char *label) : EnvelopeBase(label) {}
+
+    // TODO: int delay_length = 5;                    // D - delay before atack starts
+    unsigned int  attack_length   = 0;                // A - attack  - length of stage
+    unsigned int  hold_length     = (PPQN / 4) - 1;   // H - hold    - length to hold at end of attack before decay
+    unsigned int  decay_length    = (PPQN / 2) - 1;   // D - decay   - length of stage
+    float         sustain_ratio   = 0.90f;            // S - sustain - level to drop to after decay phase
+    unsigned int  release_length  = (PPQN / 2) - 1;   // R - release - length (time to drop to 0)
+
+    int8_t lfo_sync_ratio_hold_and_decay = 0;
+    int8_t lfo_sync_ratio_sustain_and_release = 0;
+
+    int8_t cc_value_sync_modifier = 12;
+
+    virtual void randomise() {
         //this->lfo_sync_ratio_hold_and_decay = (int8_t)random(0,127);
         //this->lfo_sync_ratio_sustain_and_release = (int8_t)random(0,127);
         this->set_attack((int8_t)random(0,64));
@@ -91,7 +190,7 @@ class EnvelopeBase {
         this->invert = (int8_t)random(0,10) < 2;
     }
 
-    void initialise_parameters() {
+    virtual void initialise_parameters() {
         //this->lfo_sync_ratio_hold_and_decay = (int8_t)random(0,127);
         //this->lfo_sync_ratio_sustain_and_release = (int8_t)random(0,127);
         this->set_attack(0);
@@ -102,17 +201,8 @@ class EnvelopeBase {
         this->invert = (int8_t)random(0,10) < 2;
     }
 
-    void kill() {
-        this->stage = OFF;
-        //this->stage_start_level = (int8_t)0;
-        this->last_state.stage = OFF;
-        this->last_state.lvl_start = 0;
-        this->last_state.lvl_now = 0;
-        send_envelope_level(0);
-    }
-
     // received a message that the state of the envelope should change (note on/note off etc)
-    void update_state (int8_t velocity, bool state, uint32_t now = ticks) {
+    virtual void update_state (int8_t velocity, bool state, uint32_t now = ticks) {
         //unsigned long now = ticks; //clock_millis(); 
         //unsigned long env_time = millis();
         if (state == true) { //&& this->stage==OFF) {  // envelope told to be in 'on' state by note on
@@ -166,13 +256,6 @@ class EnvelopeBase {
         }
     }
 
-
-    struct envelope_state_t {
-        uint8_t stage = OFF;
-        uint8_t lvl_start = 0;
-        uint8_t lvl_now = 0;
-        uint32_t elapsed = 0;
-    };
     envelope_state_t calculate_envelope_level(uint8_t stage, uint8_t stage_elapsed, uint8_t level_start, uint8_t velocity = 127) {
         float ratio = (float)PPQN / (float)cc_value_sync_modifier;  // calculate ratio of real ticks : pseudoticks
         unsigned long elapsed = (float)stage_elapsed * ratio;   // convert real elapsed to pseudoelapsed
@@ -294,65 +377,6 @@ class EnvelopeBase {
         return return_state;
     }
 
-    struct graph_t {
-        int_least8_t value = 0;
-        char stage = -1;
-    };
-    graph_t graph[240];
-
-    void calculate_graph() {
-        envelope_state_t graph_state = {
-            .stage = ATTACK,
-            .lvl_start = 0,
-            .lvl_now = 0
-        };
-        int stage_elapsed = 0;
-        for (int i = 0 ; i < 240 ; i++) {
-            envelope_state_t result = calculate_envelope_level(graph_state.stage, stage_elapsed, graph_state.lvl_start, velocity);
-            if (result.stage != graph_state.stage) {
-                graph_state.lvl_start = result.lvl_now;
-                stage_elapsed = 0;
-            } else {
-                stage_elapsed++;
-            }
-            graph[i].value = result.lvl_now;
-            graph[i].stage = result.stage;
-            graph_state.stage = result.stage;
-            if (result.stage==SUSTAIN && stage_elapsed >= PPQN) {
-                stage_elapsed = 0;
-                graph_state.stage = RELEASE;    // move to release after 1 beat, if we are calculating the graph
-            }
-        }
-    }
-
-    envelope_state_t last_state = {
-        .stage = OFF,
-        .lvl_start = 0,
-        .lvl_now = 0,
-        .elapsed = 0
-    };
-    void process_envelope(unsigned long now = millis()) {
-        //now = ticks;
-        unsigned long elapsed = now - this->stage_triggered_at;
-        //unsigned long real_elapsed = elapsed;    // elapsed is currently the number of REAL ticks that have passed
-
-        envelope_state_t new_state = calculate_envelope_level(last_state.stage, elapsed, last_state.lvl_start, velocity);
-        if (new_state.stage!=last_state.stage) {
-            //Serial.printf("process_envelope(now=%-3i) began at stage %i, changed to stage %i (elapsed is %-3i)\n", now, last_state.stage, new_state.stage, elapsed);
-            this->stage_triggered_at = now;
-        }
-
-        if (this->last_sent_actual_lvl != new_state.lvl_now) {
-            send_envelope_level(new_state.lvl_now);
-            last_sent_actual_lvl = new_state.lvl_now;
-        }
-
-        last_state.elapsed = elapsed;
-        last_state.stage = new_state.stage;
-        last_state.lvl_start = new_state.lvl_start;
-        last_state.lvl_now = new_state.lvl_now;
-    }
-
     int attack_value, hold_value, decay_value, sustain_value, release_value;
 
     virtual void set_attack(int8_t attack) {
@@ -436,9 +460,5 @@ class EnvelopeBase {
     virtual uint8_t get_cc_value_sync_modifier() {
         return this->cc_value_sync_modifier;
     }
-    
-    #ifdef ENABLE_SCREEN
-        virtual void make_menu_items(Menu *menu, int index);
-    #endif
 
 };
--- src/envelopes/envelopes.cpp
+++ src/envelopes/envelopes.cpp
@@ -6,11 +6,21 @@
     #include "submenuitem_bar.h"
     #include "mymenu/menuitems_envelopegraph.h"
 
+
     void EnvelopeBase::make_menu_items(Menu *menu, int index) {
+        char label[40];
+        snprintf(label, 40, "Envelope %i: %s", index, this->label);
+        menu->add_page(label, C_WHITE, false);
+
+        menu->add(new EnvelopeDisplay("Graph", this));
+        menu->add(new EnvelopeIndicator("Indicator", this));
+    }
+
+    void RegularEnvelope::make_menu_items(Menu *menu, int index) {
+        EnvelopeBase::make_menu_items(menu, index);
         //#ifdef ENABLE_ENVELOPE_MENUS
-            char label[40];
-            snprintf(label, 40, "Envelope %i: %s", index, this->label);
-            menu->add_page(label, C_WHITE, false);
+            menu->add(new EnvelopeDisplay("Graph", this));
+            menu->add(new EnvelopeIndicator("Indicator", this));
 
             SubMenuItemColumns *sub_menu_item_columns = new SubMenuItemColumns("Options", 5);
             sub_menu_item_columns->show_header = false;
@@ -20,6 +30,8 @@
             sub_menu_item_columns->add(new ObjectNumberControl<EnvelopeBase,int8_t>("Dec", this, &EnvelopeBase::set_decay,   &EnvelopeBase::get_decay,     nullptr, 0, 127, true, true));
             sub_menu_item_columns->add(new ObjectNumberControl<EnvelopeBase,int8_t>("Sus", this, &EnvelopeBase::set_sustain, &EnvelopeBase::get_sustain,   nullptr, 0, 127, true, true));
             sub_menu_item_columns->add(new ObjectNumberControl<EnvelopeBase,int8_t>("Rel", this, &EnvelopeBase::set_release, &EnvelopeBase::get_release,   nullptr, 0, 127, true, true));
+
+            menu->add(sub_menu_item_columns);
 
             SubMenuItemBar *typebar = new SubMenuItemBar("Type");
             typebar->show_header = false;
@@ -31,11 +43,6 @@
             typebar->add(new ObjectNumberControl<EnvelopeBase,int8_t>("Mod SR",  this, &EnvelopeBase::set_mod_sr,  &EnvelopeBase::get_mod_sr,    nullptr, 0, 127, true, true));
 
             typebar->add(new ObjectNumberControl<EnvelopeBase,uint8_t>("Sync", this, &EnvelopeBase::set_cc_value_sync_modifier, &EnvelopeBase::get_cc_value_sync_modifier, nullptr, 1, 24, true, true));
-
-            menu->add(new EnvelopeDisplay("Graph", this));
-            menu->add(new EnvelopeIndicator("Indicator", this));
-
-            menu->add(sub_menu_item_columns);
 
             menu->add(typebar);
             //menu->add(mod);
