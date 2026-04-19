06ff5ac74e1f3172ebce68cad43af7563c369b81
Tristan Rowley <doctea@gmail.com>
2026-04-19 12:09:37 +0100

    tweaked initial saveloadlib sizes -- altogether saved ~11K on Micr olidian

--- include/chord_player.h
+++ include/chord_player.h
@@ -20,7 +20,7 @@
 #endif
 class ChordPlayer 
 #ifdef ENABLE_STORAGE
-    : public SHDynamic<0, 12> // no children; selected_chord + channel settings
+    : public SHDynamic<0, 6> // no children; selected_chord + channel settings
 #endif
     {  // no children; selected_chord + channel settings
     public:
